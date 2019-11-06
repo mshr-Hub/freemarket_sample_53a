65419cc3a4f70d3a37aa4ae62885917c06006c73
mshr-Hub <masa.megane.616@gmail.com>
2019-11-06 19:01:51 +0900

    IPアドレスを変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server 'freemarket.gyabooo.work', user: 'ec2-user', roles: %w{app db web}
+server '52.69.189.4', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
